495d534f7fc16a49cd48f0fa28c479ad2f718d2c
walidmokhtari <62558067+walidmokhtari@users.noreply.github.com>
2022-01-13 22:13:24 +0100

    Update apollo-client.js

--- src/apollo/apollo-client.js
+++ src/apollo/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: `${process.env.API_URL}/graphql`,
+    uri: "http://localhost:3001/graphql",
     cache: new InMemoryCache()
 })
 
